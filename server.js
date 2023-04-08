720e9a41534c8c4f5bac57005d4688dd30e26a2c
unknown <afridarohmatin@gmail.com>
2023-04-08 12:20:38 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware untuk meng-parse body request dalam format x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Endpoint '/masuk'
+app.post('/masuk', (req, res) => {
+  // Mendapatkan nilai parameter 'id' dari body request
+  const id = req.body.id;
+  
+  // Cek apakah nilai parameter sama dengan 4263621817789128
+  if (id === '4263621817789128') {
+    res.send('1');
+  } else {
+    res.send('0');
+  }
+});
+
+// Jalankan server pada port 3000
+app.listen(3000, () => console.log('Server running on port 3000'));
