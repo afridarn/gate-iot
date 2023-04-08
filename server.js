ae8b29a77c759c34840a29c8e8c4b610143bc051
unknown <afridarohmatin@gmail.com>
2023-04-08 19:25:06 +0700

    add cors

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 443;
+const cors = require('cors');
 
 const app = express();
 
-// Middleware untuk meng-parse body request dalam format x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Endpoint '/masuk'
+app.use(cors());
+
+
 app.post('/masuk', (req, res) => {
-  // Mendapatkan nilai parameter 'id' dari body request
   const id = req.body.id;
   
-  // Cek apakah nilai parameter sama dengan 4263621817789128
   if (id === '4263621817789128') {
     res.send('1');
   } else {
@@ -20,5 +20,4 @@ app.post('/masuk', (req, res) => {
   }
 });
 
-// Jalankan server pada port 3000
 app.listen(port, () => console.log('Server running on port 443'));
